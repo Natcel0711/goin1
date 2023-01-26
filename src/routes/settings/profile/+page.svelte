1081983eb53a7ab3b5581a100b7dad09f6fc9817
Natcel0711 <natcelnieves@gmail.com>
2023-01-25 20:46:54 -0400

    align button

--- src/routes/settings/profile/+page.svelte
+++ src/routes/settings/profile/+page.svelte
@@ -2,14 +2,36 @@
     export let data
 </script>
 <form>
-    <div class="grid">
-        <label for="username">
-            Username
-            <input type="text" id="username" name="username" placeholder="Username" required />
-        </label>
-        
+    <div class="gridi">
+        <div class="asido">
+            <label for="username">
+                Username
+                <input type="text" id="username" name="username" placeholder="Username" required />
+            </label>
+        </div>
+        <div class="seccionado">
+            <a href="#" role="button" class="secondary">Save</a>
+        </div>
     </div>
-    <a href="#" role="button" class="secondary">Save</a>
 </form>
 <style>
+	a {
+		color: whitesmoke;
+	}
+	.gridi {
+		display: grid;
+		grid-template-columns: 4fr 1fr;
+		grid-template-rows: 1fr;
+		grid-column-gap: 50px;
+		grid-row-gap: 0px;
+	}
+
+	.asido {
+		grid-area: 1 / 1 / 2 / 2;
+	}
+	.seccionado {
+		grid-area: 1 / 2 / 2 / 3;
+        display: flex;
+        align-items: center;
+	}
 </style>
